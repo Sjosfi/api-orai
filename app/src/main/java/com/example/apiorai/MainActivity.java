11c0ae8f64f4616519a60a2592f7f1a64154c307
Petrik Diak <diak@petrik.hu>
2024-12-11 17:03:36 +0100

    felkesz

---
+++ app/src/main/java/com/example/apiorai/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.apiorai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewTermekek;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        listViewTermekek = findViewById(R.id.listViewTermekek);
+
+        listViewTermekek.setOnItemClickListener((parent, view, position, id) -> {
+            Termek selectedTermek = termekList.get(position);
+            Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+            intent.putExtra("termek", selectedTermek);
+            startActivity(intent);
+        });
+    }
+}
